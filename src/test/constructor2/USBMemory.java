7fe1b8c1fe02c169be8da263b4017030c0355011
kjminn <kjminn2001@gmail.com>
2022-01-07 14:12:10 +0900

    completion Consructor Test2

---
+++ src/test/constructor2/USBMemory.java
@@ -0,0 +1,24 @@
+package test.constructor2;
+
+public class USBMemory {
+	
+	private int capacity;
+	private String color;
+	
+	public USBMemory(int capacity, String color) {
+		this.capacity = capacity;
+		this.color = color;
+	}
+	
+	public String showInfo() {
+		return capacity+ "GB, " + color + "색상";
+	}
+	
+	public void writeMemory() {
+		System.out.println(showInfo() + "의 USB Memory에 데이터를 저장한다.");
+	}
+
+	public void readMemory() {
+		System.out.println(showInfo() + "의 USB Memory에 데이터를 읽어온다.");
+	}
+}
